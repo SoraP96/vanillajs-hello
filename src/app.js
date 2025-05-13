1fd18a3ac8575e2ff3d710059759d085bb42cf6e
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-05-13 20:29:06 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let sujeto = ['Un unicornio', 'Mi vecino loco', 'Un gato ninja', 'El repartidor de pizza'];
+let accion = ['escondió', 'quemó', 'pisó', 'lanzó'];
+let objeto = ['mi mochila', 'los deberes', 'mi ordenador', 'el uniforme'];
+let cuando = ['antes del desayuno', 'mientras dormía', 'durante la tormenta', 'en la clase de yoga'];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".alert").innerHTML = `${sujeto[Math.floor(Math.random() * sujeto.length)]}` + " " + `${accion[Math.floor(Math.random() * accion.length)]}` + " " + `${objeto[Math.floor(Math.random() * objeto.length)]}` + " " + `${cuando[Math.floor(Math.random() * cuando.length)]}` + ".";
 };
